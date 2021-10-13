49508fb8c6bf443372f5b49eddeae515ad4b3979
CaroPass <carola@passardi@gmail.com>
2021-10-13 12:47:11 -0300

    clase Perro en rama nueva

---
+++ PracticaGit/src/practicagit/Perro.java
@@ -0,0 +1,19 @@
+
+package practicagit;
+
+public class Perro {
+    private String nombre;
+
+    public Perro() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
